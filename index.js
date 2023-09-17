8819783af249881446fd1f497915add7f90539fe
Murtaza Raja <murt.raja@gmail.com>
2023-09-17 21:36:51 +0530

    creating /check route

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.get("/check", (req, res) => {
+  if (fs.existsSync("a_folder")) {
+    res.send("Exists");
+  } else {
+    fs.mkdirSync("a_folder");
+    res.send("Created the folder");
+  }
+});
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+app.listen(process.env.PORT || 3000);
